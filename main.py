0fd2285c33e643bb5602fb2c0ff122ee5891edf5
Professor Igor <trabalho.computador.sjn@gmail.com>
2024-05-08 15:05:42 -0300

    alteração no main

--- main.py
+++ main.py
@@ -1,19 +1,25 @@
+import os
+
+# Configurar para usar modelos menores e offload para CPU
+os.environ["SUNO_USE_SMALL_MODELS"] = "True"
+os.environ["SUNO_OFFLOAD_CPU"] = "True"
+
 from bark import SAMPLE_RATE, generate_audio, preload_models
 from scipy.io.wavfile import write as write_wav
 from IPython.display import Audio
 
-# download and load all models
+# Carrega todos os modelos na memória (deverá usar menos VRAM com as configurações acima)
 preload_models()
 
-# generate audio from text
+# Gera áudio a partir do texto
 text_prompt = """
-     Hello, my name is Suno. And, uh — and I like pizza. [laughs] 
-     But I also have other interests such as playing tic tac toe.
+      Olá, meu nome é Cronos. E, ah - eu gosto de pizza. [rir]
+     Mas eu também tenho outros interesses, como jogar jogo da velha.
 """
-audio_array = generate_audio(text_prompt)
+audio_array = generate_audio(text_prompt, history_prompt="v2/pt_speaker_8")
 
-# save audio to disk
+# Salva o áudio em disco
 write_wav("bark_generation.wav", SAMPLE_RATE, audio_array)
-  
-# play text in notebook
+
+# Reproduz o texto no notebook
 Audio(audio_array, rate=SAMPLE_RATE)
